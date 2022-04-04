9c7dffb644ca7ebe810baffa1225310598de0e95
ashrafali-v <ashrafalifrk@gmail.com>
2022-04-04 15:08:40 +0800

    first heroku deployment

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 //Import Routes
 // const authRoutes = require('./routes/auth');
-// const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 // const postRoutes = require('./routes/post');
 
-
 dotenv.config();
-
+app.use(bodyParser.json());
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
-    console.log('DB connected') 
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("DB connected")
 );
-
 
 //Route Middlewares
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-app.post('/login',(req,res)=>{
-    const username = req.body.name;
-    console.log(username);
-    const user ={name:username};
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.post("/login", (req, res) => {
+  const username = req.body.name;
+  console.log(username);
+  const user = { name: username };
 
-    const accessToken =     jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
-    res.write
-    res.json({ accessToken:accessToken });
-
-})
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.write;
+  res.json({ accessToken: accessToken });
+});
 
 // function authenticateToken(req,res,next){
 //     const authHeader = req.headers['authorization']
@@ -47,10 +47,10 @@ app.post('/login',(req,res)=>{
 //     })
 // }
 //app.use('/api/auth', authRoutes);
-//app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 //app.use('/api/post', postRoutes);
 
-require('./router')(app);
+require("./router")(app);
 //start listening the server
-app.listen(3027, () => console.log('Server is Up and running'+ __dirname));
+app.listen(3027, () => console.log("Server is Up and running" + __dirname));
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,64 +1,64 @@
-const router = require('express').Router();
-const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const router = require("express").Router();
+const User = require("../app/model/User");
+const { registerValidation } = require("../validation");
 
-router.get('/', async (req, res) => {
-    //res.send("hiiii");
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
-router.get('/:userId', async (req, res) => {
-    //res.send("hiiii");
-    try {
-        const user = await User.findById(req.params.userId);
-        res.json(user);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+router.get("/:userId", async (req, res) => {
+  //res.send("hiiii");
+  try {
+    const user = await User.findById(req.params.userId);
+    console.log(user);
+    let c = res.json(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
-router.delete('/:userId', async (req, res) => {
-    //res.send("hiiii");
-    try {
-        const user = await User.remove({_id:req.params.userId});
-        res.json(user);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+router.delete("/:userId", async (req, res) => {
+  //res.send("hiiii");
+  try {
+    const user = await User.remove({ _id: req.params.userId });
+    res.json(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
-router.patch('/:userId', async (req, res) => {
-    //res.send("hiiii");
-    try {
-        const updatedUser = await User.updateOne(
-            {_id:req.params.userId},
-            {$set:{name:req.body.name}}
-        );
-        res.json(updatedUser);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+router.patch("/:userId", async (req, res) => {
+  //res.send("hiiii");
+  try {
+    const updatedUser = await User.updateOne(
+      { _id: req.params.userId },
+      { $set: { name: req.body.name } }
+    );
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
-router.post('/register', async (req, res) => {
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    //if the user already there
-    const emailExist = await User.findOne({email:req.body.email});
-    if(emailExist) return res.status(400).send("User already exist");
+  //if the user already there
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("User already exist");
 
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try {
-        const savedUser = await user.save();
-        res.json(savedUser);
-    } catch (err) {
-        res.json({Message:err});
-    }
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ Message: err });
+  }
 });
 module.exports = router;
