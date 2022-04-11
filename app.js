2169794ffc756e665739d1cb611a9c75bcc6fba5
ashrafali-v <ashrafalifrk@gmail.com>
2022-04-12 07:55:53 +0800

    add domain https://test-hackothon.herokuapp.com/

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
     "http://localhost:3000",
-    "http://localhost:3027"
+    "http://localhost:3027",
+    "https://test-hackothon.herokuapp.com/"
   ); // update to match the domain you will make the request from
   //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
